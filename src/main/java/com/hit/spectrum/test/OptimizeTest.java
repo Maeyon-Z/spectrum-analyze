9866d157e6c715c457d3a7786af479fb33e91d96
zmy <maeyonzzzz@gmail.com>
2023-12-05 08:49:23 +0800

    实现优化

--- src/main/java/com/hit/spectrum/test/OptimizeTest.java
+++ src/main/java/com/hit/spectrum/test/OptimizeTest.java
@@ -2,6 +2,8 @@ package com.hit.spectrum.test;
 
 import org.apache.commons.math3.linear.*;
 
+import java.util.Arrays;
+
 
 public class OptimizeTest {
 
@@ -22,7 +24,7 @@ public class OptimizeTest {
     private static double calculateDelta(RealMatrix A, RealVector y, RealVector x, double lambda) {
         // 计算 ||Ax - y||_2
         RealVector AxMinusY = A.preMultiply(x).subtract(y);
-        double term1 = Math.sqrt(AxMinusY.dotProduct(AxMinusY));
+        double term1 = AxMinusY.getNorm();
 
         // 计算 λ||x||_1
         double term2 = lambda * x.getL1Norm();
@@ -39,27 +41,31 @@ public class OptimizeTest {
 
     private static RealVector calculateGradient(RealMatrix A, RealVector y, RealVector x, double lambda, double mu) {
         // 计算梯度
-        RealVector AxMinusY = A.preMultiply(x).subtract(y);
-        return A.operate(AxMinusY).mapMultiply(1.0 / AxMinusY.getNorm()).add(
-                x.mapMultiply(lambda)).subtract(x.mapMultiply(mu).ebeDivide(x)
-        );
+        RealVector term1 = A.transpose().preMultiply(A.preMultiply(x).subtract(y));
+
+        double[] x1 = new double[x.toArray().length];
+        Arrays.fill(x1, lambda);
+        RealVector term2 = new ArrayRealVector(x1);
+
+        for (int i = 0; i < x1.length; i++) x1[i] = mu / x1[i];
+        RealVector term3 = new ArrayRealVector(x1);
+
+        return term1.add(term2).subtract(term3);
     }
 
-    private static RealMatrix calculateHessian(RealMatrix A, RealVector x) {
-        // 计算hessian矩阵
-        RealMatrix hessianPhi = A.transpose().preMultiply(A).scalarMultiply(1.0 / x.getNorm());
+    private static RealMatrix calculateHessian(RealMatrix A, RealVector x, double mu) {
+        RealMatrix term1 = A.multiply(A.transpose());
 
-        // 添加对角块
-        RealVector xInv = x.mapMultiply(1.0 / x.getNorm());
-        hessianPhi = hessianPhi.add(MatrixUtils.createRealDiagonalMatrix(xInv.toArray()));
+        double[] x1 = x.toArray();
+        for (int i = 0; i < x1.length; i++) x1[i] = -1 * mu / Math.sqrt(x1[i]);
+        RealMatrix term2 = MatrixUtils.createRealDiagonalMatrix(x1);
 
-        return hessianPhi;
+        return term1.add(term2);
     }
 
     private static RealVector solveLinearSystem(RealMatrix A, RealVector b) {
-        // 使用Apache Commons Math库中的线性系统求解器解线性方程组
-        DecompositionSolver solver = new LUDecomposition(A).getSolver();
-        return solver.solve(b);
+        RealMatrix inverse = new LUDecomposition(A).getSolver().getInverse();
+        return inverse.preMultiply(b);
     }
 
     private static boolean judgeX(double[] x){
@@ -86,6 +92,7 @@ public class OptimizeTest {
 
         RealMatrix A = new Array2DRowRealMatrix(A1);
         RealVector y = new ArrayRealVector(y1);
+
         // 设定初始解x、循环初始次数k、初始步长alpha、步长缩减因子s、控制参数c、最大迭代次数、结束阈值、稀疏因子lambda
         RealVector x = new ArrayRealVector(A.getRowDimension(), 1.0);
         int k = 1;
@@ -96,18 +103,23 @@ public class OptimizeTest {
 
         // 开始迭代优化
         while (k < maxIterations){
+            alpha = 1.0;
             // 如果 delta < tolerance 则结束，x为最终结果，否则继续计算
             double delta = calculateDelta(A, y, x, lambda);
             if(delta < tolerance) break;
 
+            System.out.println(k + "-" + delta);
+
             // 确定迭代方向
             RealVector gradPhi = calculateGradient(A, y, x, lambda, delta/A.getRowDimension());
-            RealMatrix hessianPhi = calculateHessian(A, x);
+            RealMatrix hessianPhi = calculateHessian(A, x, delta/A.getRowDimension());
             RealVector dx = solveLinearSystem(hessianPhi, gradPhi);
+
             dx.mapMultiplyToSelf(-1.0);
             // 判断步长是否满足要求
             while (!(judgeX(x.toArray()) && judgeAlpha(x, alpha, dx, c, gradPhi, A, y, delta, lambda)))
                 alpha *= s;
+
             x = x.add(dx.mapMultiply(alpha));
 
             k++;
@@ -115,5 +127,11 @@ public class OptimizeTest {
         return x.toArray();
     }
 
+    private static boolean judge(double[] x, double[] dx, double alpha) {
+        for (int i = 0; i < x.length; i++) {
+            if((x[i] + dx[i] * alpha) < 0) return false;
+        }
+        return true;
+    }
 
 }
